df24a6c766de9a766c2533c9460db6f5f9d7b885
HansenWANG <1343113350@qq.com>
2024-05-21 14:14:48 +0800

    fix: 代码提交

--- src/api/index.js
+++ src/api/index.js
@@ -7,26 +7,26 @@ import http from './httpWrapper';
 
 console.log(process.env, 'process.env');
 
-export const chatKnowledgeSSEPath = `${process.env.REACT_APP_ROUTER_BASE_URL}/third-parties/chat-knowledge`;
+export const chatKnowledgeSSEPath = `${process.env.REACT_APP_API_BASE_URL}/third-parties/chat-knowledge`;
 
 export const queryInterviewInfo = (params, config) => {
-  return http.post(`${process.env.REACT_APP_ROUTER_BASE_URL}/no-auth/query-interview-info`, params, config);
+  return http.post(`${process.env.REACT_APP_API_BASE_URL}/no-auth/query-interview-info`, params, config);
 };
 
 
 export const abortChatKnowledge = (params) => {
   return http.get(
-    `${process.env.REACT_APP_ROUTER_BASE_URL}/third-parties/abort`,
+    `${process.env.REACT_APP_API_BASE_URL}/third-parties/abort`,
     params
   );
 };
 
 export const prefixDataBeforeSSE = (params) => {
   return http.post(
-    `${process.env.REACT_APP_ROUTER_BASE_URL}/third-parties/prefix-data-before-sse`,
+    `${process.env.REACT_APP_API_BASE_URL}/third-parties/prefix-data-before-sse`,
     params
   );
 };
 export const emailToAuthor = (params, config) => {
-  return http.post(`${process.env.REACT_APP_ROUTER_BASE_URL}/no-auth/email-to-author`, params, config);
+  return http.post(`${process.env.REACT_APP_API_BASE_URL}/no-auth/email-to-author`, params, config);
 };
